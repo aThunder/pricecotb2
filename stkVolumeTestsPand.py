95b9a458d2e0c02cd03dcb903bc7de053cd8e458
aThunder <rr234rr@gmail.com>
2016-03-23 22:30:36 -0400

    major functionality upgrades to volume indicators

--- stkVolumeTestsPand.py
+++ stkVolumeTestsPand.py
@@ -1,12 +1,12 @@
 # cotsqlqPandas.py
 
 '''
-    1. Queries ????2 SQLite tables (COT & ETFs) and uses Join for results
-    2. Performs several calculations and summaries on results
-    3. Weekly data only
-    4.
-'''
+    1. Queries SQLite table for symbol and date range specified by user
+    2. Performs several volume calculations:
+            a: On-Balance Volume
+            b: Avg Up v. Down volume
 
+'''
 
 import sqlite3
 import pandas as pd
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 
-class spCOT():
+class stkVolume():
 
     def __init__(self):
         self.conn = sqlite3.connect('allStks.db')
@@ -23,18 +23,46 @@ class spCOT():
         self.diskEngine = create_engine('sqlite:///allStks.db')
         self.recentList =[]
 
-    def checkStkData(self,symbol,IDKEY):
-        # self.symbol = symbol
-        self.symbol = ('spy','aapl')
-        print('ID KEY: ',IDKEY,self.symbol)
-        self.stkDataDetail = pd.read_sql_query("SELECT * "
-                                               "FROM SymbolsDataDaily "
-                                               "WHERE ID_NAMEKEY = {0} "
-                                               "AND DATE >= '2016-03-05' "
-                                               "AND SYMBOL IN ('aapl') "
-                                               " ".format(IDKEY,self.symbol),self.diskEngine)
-        # print("StkDataDetail: ", self.stkDataDetail[['date','Symbol','open','high','low','close','vol']])
+    # def numberDaysToDate(self,numberOfDays):
+    #     self.numberOfDays = numberOfDays
+    #     print("DaysCounter: ",self.numberOfDays)
 
+        # dateadd(-5)
+        # current date getdate()
+
+
+    def retrieveStkData(self,symbol,IDKEY,startDate,numberDays):
+        self.symbol = symbol
+        # self.symbol = ('aapl')
+        self.startDate = startDate
+        self.numberDays = numberDays + 1
+
+        print('ID KEY: ',IDKEY,self.symbol)
+        # self.stkDataDetail = pd.read_sql_query("SELECT * "
+        #                                        "FROM SymbolsDataDaily "
+        #                                        "WHERE ID_NAMEKEY = {0} "
+        #                                        "AND DATE >= '2016-01-05' "
+        #                                        "AND SYMBOL IN ('aapl')"
+        #                                        "AND ID > 5 "
+        #                                        " ".format(IDKEY,self.symbol),self.diskEngine)
+
+        # self.stkDataDetail = pd.read_sql_query("SELECT SYMBOL, date('now','-1 day'),CLOSE,date " #max(DATE), CLOSE " #date('now','-1 day'), CLOSE " # date('now') - 1 day "#max(date)-1,SYMBOL,CLOSE "
+        #                                        "FROM SymbolsDataDaily "
+        #                                        "WHERE SYMBOL IN ('aapl') "
+        #                                        " AND date('now',-5 days) "
+        #                                        " ".format(IDKEY,self.symbol),self.diskEngine)
+        #
+        self.stkDataDetail = pd.read_sql_query("SELECT SYMBOL,DATE,CLOSE,VOL "
+                                               "FROM (SELECT * FROM SymbolsDataDaily "
+                                               "WHERE SYMBOL IN ('{0}')"
+                                               "ORDER BY DATE DESC LIMIT {1}) "
+                                               "ORDER BY DATE ASC "
+                                               " ".format(self.symbol,self.numberDays),self.diskEngine)
+
+
+
+        # print("StkDataDetail: ", self.stkDataDetail[['ID','date','Symbol','open','high','low','close','vol']])
+        print("StkDataDetail: ", self.stkDataDetail)
         print()
         self.df = self.stkDataDetail
 
@@ -52,10 +80,10 @@ class spCOT():
     def orderData(self):
         # print("Close: ",self.df['close'])
         # print("SortClose: ",self.df.sort('close'))
-        movavg = pd.rolling_mean(self.df['vol'],50)
+        movavg = pd.rolling_mean(self.df['vol'],3)
         periodchg = self.df['close'].diff(2)
-        print("MovAvg: ",movavg)
-        # print("Change: ",periodchg)
+        print("VolMovAvg: ",movavg)
+        print("CloseChange: ",periodchg)
 
     def priceVolStats(self):
         self.df['changeClose'] = self.df['close'].diff()
@@ -79,25 +107,23 @@ class spCOT():
 
     def onBalanceVolume(self):
         self.runningVol = 0
-        # self.df['runVol'] = self.df['vol'][self.volMaskUp]# - self.df['vol'][self.volMaskDn]
-        # print('RVTest: ',self.df['runVol'])
         # # print("RunningVolume: ", self.df[['date','Symbol','close','changeClose','vol','runVol']])
         counter = 0
         for i in self.df['close'].diff():
-            print("i: ",i)
-            print("counter: ", counter)
-            print(self.df['date'][counter])
-
-            print("VRunItem: ",self.df['vol'][counter])
+            # print("i: ",i)
+            # print("counter: ", counter)
+            # print(self.df['date'][counter])
+            #
+            # print("VRunItem: ",self.df['vol'][counter])
 
             if i > 0 and counter > 0:
-                print("YES")
+                # print("YES")
                 self.runningVol += self.df['vol'][counter]
-                print("RunningVolPlus: ", self.runningVol)
+                print("OBVPlus: ", self.df['date'][counter],self.runningVol)
             elif i < 0 and counter > 0:
-                print("NO")
+                # print("NO")
                 self.runningVol -= self.df['vol'][counter]
-                print("RunningVolMinus: ", self.runningVol)
+                print("OBVMinus: ", self.df['date'][counter],self.runningVol)
             print()
 
 
@@ -105,38 +131,67 @@ class spCOT():
 
         # print(self.df['date'][5:8])
 
-    def avgVolUpDown(self):
+    def avgVolumeUpDown(self):
         self.upVol = []
         self.dnVol = []
         totalUp = 0
+        totalDn = 0
         counter = 0
 
         for i in self.df['close'].diff():
-            print("i: ",i)
-            print("counter: ", counter)
-            print(self.df['date'][counter])
-
-            print("VRunItem: ",self.df['vol'][counter])
+            # print("i: ",i)
+            # print("counter: ", counter)
+            # print(self.df['date'][counter])
+            #
+            # print("VRunItem: ",self.df['vol'][counter])
 
             if i > 0 and counter > 0:
-                print("YES")
+                # print("YES")
                 self.upVol.append(self.df['vol'][counter])
-                print("upVol: ", self.upVol)
+                # print("upVol: ", self.upVol)
             elif i < 0 and counter > 0:
-                print("NO")
+                # print("NO")
                 self.dnVol.append(self.df['vol'][counter])
-                print("dnVol: ", self.dnVol)
-            print()
+                # print("dnVol: ", self.dnVol)
+            # print()
 
             counter += 1
 
         for i in self.upVol:
             totalUp += i
         upAvg = totalUp/len(self.upVol)
-        print('upMean: ', upAvg)
+        print('upVolumeMean: ', upAvg)
+        print("UpDaysCount: ",len(self.upVol))
+
 
         upVolNP = np.mean(self.upVol)
-        print("upVolNP: ", upVolNP)
+        print("upVolumeMeanNP: ", upVolNP)
+        print()
+
+
+        for i in self.dnVol:
+            totalDn += i
+        dnAvg = totalDn/len(self.dnVol)
+        print('downVolumeMean: ', dnAvg)
+        print("DownDaysCount: ",len(self.dnVol))
+
+        dnVolNP = np.mean(self.dnVol)
+        print("downVolumeMeanNP: ", dnVolNP)
+
+        print()
+        print("Up:Down Volume Avg: ", upVolNP/dnVolNP)
+        print("Up:Down Volume Days: ",len(self.upVol)/len(self.dnVol))
+
+    def priceMove(self):
+        # print(self.df['close'])
+        print()
+        print("{0} days observations: ".format(self.numberDays-1))
+        mostRecentPrice = self.df['close'][self.numberDays-1]
+        firstPrice = self.df['close'][1]
+        # print()
+        print("First,Last: ",firstPrice, mostRecentPrice)
+        print("PriceChange: ",mostRecentPrice-firstPrice)
+        print("% Change: ", ((mostRecentPrice-firstPrice)/firstPrice)*100)
 
     def grouping(self):
         group1 = self.df.groupby()
@@ -144,54 +199,6 @@ class spCOT():
 
 
 
-    def innerJoin1(self,criteria1,startDate):
-        self.criteria1 = criteria1
-        # startDate = '2015-11-01'
-        print('startDate: ', startDate)
-        self.intoPandasJoin1 = pd.read_sql_query("SELECT DataCOT.NAME,"
-                                                 " DataCOT.OPENINT, "
-                                                 "DataCOT.DATE,"
-                                                 " DataCOT.RptableLong,"
-                                                 " DataCOT.RptableShort,"
-                                                 " CotEtfDataWeekly.SYMBOL,"
-                                                 " CotEtfDataWeekly.CLOSE,"
-                                                 " CotEtfDataWeekly.VOL, "
-                                                 " CotEtfDataWeekly.DATE "
-                                                 " FROM DataCOT "
-                                                 "INNER JOIN CotEtfDataWeekly "
-                                                 "ON DataCOT.ID_NAMEKEY =  CotEtfDataWeekly.ID_NAMEKEY "
-                                                 "AND DataCOT.DATE = CotEtfDataWeekly.DATE "
-                                                 "WHERE DataCOT.DATE > '{0}' "
-                                                 "AND NAME LIKE '{1}'"
-                                                 "ORDER BY CotEtfDataWeekly.DATE asc".format(startDate,self.criteria1),
-                                                 self.diskEngine)
-
-        countLinesAll = self.intoPandasJoin1['Date'].count()
-        self.intoPandasJoin1['NetReportable'] = self.intoPandasJoin1['RptableLong']-self.intoPandasJoin1['RptableShort']
-        self.intoPandasJoin1['WkNetRptableChg'] = self.intoPandasJoin1['NetReportable'].diff()
-        self.intoPandasJoin1['WkPriceChg'] = np.round(self.intoPandasJoin1['close'].diff(),decimals=2)
-
-        print("JOINED: ",self.intoPandasJoin1)
-
-    def mostRecent(self):
-
-        countLines = self.intoPandasJoin1['WkNetRptableChg'].count()
-        # print('#Lines: ',countLines)
-        mostRecent = ("{0}: NetReportable: {1}  WeeklyChg: {2} WkPriceChg: {3}".
-                format(self.intoPandasJoin1['Symbol'][countLines],self.intoPandasJoin1['NetReportable'][countLines],
-                self.intoPandasJoin1['WkNetRptableChg'][countLines],self.intoPandasJoin1['WkPriceChg'][countLines]))
-
-        # print("MostRecent {0}: {1}".
-        #         format(self.criteria1,mostRecent))
-
-        self.recentList.append(mostRecent)
-
-    def summary1(self):
-        print()
-        counter=1
-        for i in self.recentList:
-            print(counter, i)
-            counter +=1
 
     def plot1(self):
         print("Now plotting")
@@ -205,20 +212,24 @@ class spCOT():
     #         # db.execute('insert into test(t1, i1) values(?,?)', ('one', 1)) ## sample for format syntax
 
 def main():
-    a = spCOT()
+    a = stkVolume()
+    numberOfDays = 10
+    # a.numberDaysToDate(numberOfDays)
     # criteria5 = ['%S&P%','%Gold%','%Bond%','%Oil%']
-    criteria5 = ['spy','aapl']
+    criteria5 = ['aapl','gld']
     # startDate = input("Enter start date (YYYY-MM-DD): ") ## commented out for testing only
     startDate = '2015-10-01' ## for testing expediting only
-    for i in range(1): #criteria5:
-        a.checkStkData(i,99)
+
+    for i in criteria5:
+        a.retrieveStkData(i,99,startDate,15)
         # a.volumeChg()
         # a.mask1()
         # a.orderData()
-        # a.grouping()
-        a.priceVolStats()
+        # # a.grouping()
+        # # a.priceVolStats()
         a.onBalanceVolume()
-        a.avgVolUpDown()
+        a.avgVolumeUpDown()
+        a.priceMove()
         # a.checkEtfData(1)
         # a.innerJoin1(i,startDate)
         # a.mostRecent()
@@ -227,5 +238,9 @@ def main():
 
 if __name__ == '__main__': main()
 
+
+
+################################
+################################
 
 
